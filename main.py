368226490426ff077fc2ec83aab09fbcea55317e
Brylie Christopher Oxley <brylie@amble.fi>
2022-08-18 10:52:49 +0300

    Add GET /filter endpoint to filter dictionary keys

--- main.py
+++ main.py
@@ -11,3 +11,14 @@ async def root():
 async def read_items(items: list[str] | None = Query(default=None)):
     query_items = {"items": items}
     return query_items
+
+@app.get("/filter/")
+async def filter_keys(keys: list[str] | None = Query(default=None)):
+    initial_dict = {
+        "one": 1,
+        "two": 2,
+    }
+
+    filtered_dict = { key: initial_dict[key] for key in keys }
+
+    return filtered_dict
